d8333647daaf2c45b417f902210a70a95c627082
shshsjjd <saad_sadness@mail.ru>
2022-09-08 20:46:18 +0300

    work

---
+++ Поверёнок/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Поверёнок
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            one.Background = new SolidColorBrush(Color.FromRgb(255, 204, 153));
+            guest.Background = new SolidColorBrush(Color.FromRgb(255, 204, 153));
+            sign.Background = new SolidColorBrush(Color.FromRgb(204, 102, 0));
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1(); 
+            window1.Show();
+           
+        }
+    }
+}
